ba4299105fdca7f82abbdee2aca5a871113782c2
Murakami <murakami.edson@gmail.com>
2023-05-09 22:12:20 -0300

    atualizacao funcionario

--- src/main/java/com/cafe/modelo/Funcionario.java
+++ src/main/java/com/cafe/modelo/Funcionario.java
@@ -3,22 +3,15 @@ package com.cafe.modelo;
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
 
-import java.util.Calendar;
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.Transient;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
 
 import org.hibernate.annotations.CreationTimestamp;
@@ -26,14 +19,12 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.extern.log4j.Log4j;
 
 /**
  * @author isabella
  *
  */
 
-@Log4j
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
