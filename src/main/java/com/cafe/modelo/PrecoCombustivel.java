0b8e5a0b30cf4cfbb11377e7b52af75c9393b1d8
Murakami <murakami.edson@gmail.com>
2023-06-14 18:18:27 -0300

    preco combustivel e despesa maquina  2

---
+++ src/main/java/com/cafe/modelo/PrecoCombustivel.java
@@ -0,0 +1,55 @@
+package com.cafe.modelo;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.PositiveOrZero;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.cafe.modelo.enums.TipoCombustivel;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class PrecoCombustivel {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private Long tenant_id;
+	
+	@PositiveOrZero
+	private BigDecimal valor1 = new BigDecimal(0);
+	@PositiveOrZero
+	private BigDecimal valor2 = new BigDecimal(0);
+	
+	@Enumerated(EnumType.STRING)
+	private TipoCombustivel tipoCombustivel;
+
+
+	/*
+	 * Datas de Criação e Modificação
+	 */
+	
+	@CreationTimestamp	
+	@Column(columnDefinition = "datetime")
+	private OffsetDateTime dataCriacao;
+	
+	@UpdateTimestamp
+	@Column(columnDefinition = "datetime")
+	private OffsetDateTime dataModificacao;
+}
