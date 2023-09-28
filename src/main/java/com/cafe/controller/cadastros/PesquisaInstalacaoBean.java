83f36889f2d45318b127980466c836921bdb5c43
Murakami <murakami.edson@gmail.com>
2023-09-28 13:05:13 -0300

    reestruturacao de organização do código e atualização do modelo

---
+++ src/main/java/com/cafe/controller/cadastros/PesquisaInstalacaoBean.java
@@ -0,0 +1,57 @@
+package com.cafe.controller.cadastros;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.cafe.controller.LoginBean;
+import com.cafe.modelo.Instalacao;
+import com.cafe.service.InstalacaoService;
+import com.cafe.util.MessageUtil;
+import com.cafe.util.NegocioException;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+* @author isabella
+*
+*/
+@Getter
+@Setter
+@Named
+@ViewScoped
+public class PesquisaInstalacaoBean implements Serializable {
+	
+private static final long serialVersionUID = 1L;
+	
+	private List<Instalacao> instalacoes = new ArrayList<>();
+	private Instalacao instalacaoSelecionado;
+	
+	@Inject
+	InstalacaoService instalacaoService;
+	@Inject
+	private LoginBean loginBean;
+
+		
+	@PostConstruct
+	public void inicializar() {
+		instalacoes = instalacaoService.buscarInstalacoesPorUnidade(loginBean.getUsuario().getUnidade() ,loginBean.getTenantId());
+	}
+	
+	public void excluir() {
+		try {
+			instalacaoService.excluir(instalacaoSelecionado);			
+			this.instalacoes.remove(instalacaoSelecionado);
+			MessageUtil.sucesso("Instalação " + instalacaoSelecionado.getNome() + " excluída com sucesso.");
+		} catch (NegocioException e) {
+			e.printStackTrace();
+			MessageUtil.erro(e.getMessage());
+		}
+	}
+}
